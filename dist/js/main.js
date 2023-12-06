abddddf093ca836a709ce7616a4f9396809f26ec
gbura <rage6999@wp.pl>
2023-12-06 18:06:35 +0100

    start

---
+++ dist/js/main.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', () => {
+	fetch('http://localhost:3000/tasks')
+		.then(response => response.json())
+		.then(data => renderTasks(data))
+		.catch(error => console.error('Błąd przy pobieraniu zadań:', error))
+})
+
+function renderTasks(tasks) {
+	const taskList = document.getElementById('task-list')
+	taskList.innerHTML = ''
+	tasks.forEach(task => {
+		const taskItem = document.createElement('li')
+		taskItem.className = 'p-2 flex justify-between items-center mb-2'
+		taskItem.setAttribute('data-id', task.id)
+
+		const text = document.createElement('span')
+		text.textContent = task.title + (task.completed ? ' (Zakończono)' : ' (Niewykonane)')
+		text.className = task.completed ? 'text-green-500' : 'text-red-500'
+
+		const buttons = document.createElement('div')
+
+		const completeButton = document.createElement('button')
+		completeButton.textContent = 'Wykonaj'
+		completeButton.className = 'btn btn-complete'
+		completeButton.setAttribute('data-action', 'complete')
+
+		const editButton = document.createElement('button')
+		editButton.textContent = 'Edytuj'
+		editButton.className = 'btn btn-edit'
+		editButton.setAttribute('data-action', 'edit')
+
+		const deleteButton = document.createElement('button')
+		deleteButton.textContent = 'Usuń'
+		deleteButton.className = 'btn btn-delete'
+		deleteButton.setAttribute('data-action', 'delete')
+
+		buttons.appendChild(completeButton)
+		buttons.appendChild(editButton)
+		buttons.appendChild(deleteButton)
+
+		taskItem.appendChild(text)
+		taskItem.appendChild(buttons)
+
+		taskList.appendChild(taskItem)
+	})
+
+	// Ustawienie nasłuchiwacza zdarzeń na elemencie <ul>
+	taskList.addEventListener('click', handleTaskListClick)
+}
+
+function handleTaskListClick(event) {
+	const taskItem = event.target.closest('li[data-id]')
+	const taskId = taskItem.getAttribute('data-id')
+	if (event.target.matches('[data-action=complete]')) {
+		completeTask(taskId)
+	} else if (event.target.matches('[data-action=edit]')) {
+		editTask(taskId)
+	} else if (event.target.matches('[data-action=delete')) {
+		deleteTask(taskId)
+	}
+}
+
+function completeTask(id) {
+	console.log('Wykonano zadanie o ID:', id)
+}
+
+function editTask(id) {
+	console.log('Edycja zadania o ID:', id)
+	// Tutaj logika do edycji zadania
+}
+
+function deleteTask(id) {
+	console.log('Usunięcie zadania o ID:', id)
+	// Tutaj logika do usuwania zadania
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat', 'sans-serif'],
+			},
+			colors: {
+				deepDark: 'rgb(26, 26, 26)',
+				lightDark: 'rgb(79, 82, 92)',
+				lightBlue: 'rgb(0, 132, 255)',
+				darkBlue: 'rgb(2, 84, 161)',
+				light: 'rgb(245, 245, 245)',
+				edit: 'rgb(15, 179, 69)',
+			},
+			boxShadow: {
+				todoShadow: '4px 4px 5px rgba(82, 82, 82, 0.4)',
+			},
+		},
+	},
+	plugins: [],
+}
